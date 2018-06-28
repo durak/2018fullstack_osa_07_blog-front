1d28fd00cd3e61db619a4e08bf61e48413e95faf
durak <itapkos@gmail.com>
2018-06-28 15:11:54 +0300

    blogCommentForm validation

--- src/components/BlogCommentForm.js
+++ src/components/BlogCommentForm.js
@@ -1,19 +1,31 @@
 import React from 'react'
 
 const BlogCommentForm = ({ handleComment }) => {
+
+  const valid = (comment) => {
+    return comment.length > 0
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
     const comment = event.target.comment.value
-    event.target.comment.value = ''
 
+    if (!valid(comment)) {
+      event.target.valid.value = 'required'
+      return
+    }
+
+    event.target.comment.value = ''
+    event.target.valid.value = ''
     handleComment(comment)
   }
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
         <div>
           <input type="text" name="comment" />
+          <output name="valid"></output>
         </div>
         <button type="submit">add comment</button>
       </form>
